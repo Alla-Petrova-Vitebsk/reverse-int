c635cc7feaf9f41361ccbaa89c822e624371c6f8
Alla-Petrova-Vitebsk <alla_petrova@mail.ru>
2021-08-14 17:01:48 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
-}
+ module.exports = function reverse(n) {
+     if (n < 0) n *= -1;
+     let str = n.toString();
+     let arr = str.split('').reverse().join('');
+     return (arr);
+ }
